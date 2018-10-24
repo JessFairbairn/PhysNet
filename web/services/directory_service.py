3428350467c80c96b53126674a0d0e1ac13e72b4
Jessica Fairbairn <jess.fairbairn@postgrad.manchester.ac.uk>
2018-10-24 23:28:15 +0100

    moved results file to static directory

--- web/services/directory_service.py
+++ web/services/directory_service.py
@@ -1,7 +1,7 @@
 import json
 
 def get_verb(verb:str):
-    with open("../results.json", "r") as tempFile:
+    with open("static/results.json", "r") as tempFile:
         directory = json.load(tempFile)
         
         return directory.get(verb)
