a8161ff73f525e400dff8eaeba5de3bc1da09a73
Jessica Fairbairn <jess.fairbairn@postgrad.manchester.ac.uk>
2018-10-24 23:34:57 +0100

    attempt to fix file opening

--- web/services/directory_service.py
+++ web/services/directory_service.py
@@ -1,7 +1,7 @@
 import json
 
 def get_verb(verb:str):
-    with open("static/results.json", "r") as tempFile:
+    with open("web/static/results.json", "r") as tempFile:
         directory = json.load(tempFile)
         
         return directory.get(verb)
