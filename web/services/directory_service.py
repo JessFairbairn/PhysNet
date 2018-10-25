60849b39babb05505a0d28eb64b5aa4f04038e07
Jessica Fairbairn <jess.fairbairn@postgrad.manchester.ac.uk>
2018-10-26 00:36:52 +0100

    Display directory in decending order of tf-idf

--- web/services/directory_service.py
+++ web/services/directory_service.py
@@ -1,5 +1,6 @@
 import json
 import os
+import typing
 
 def get_verb(verb:str):
     static = _get_static_folder()
@@ -11,9 +12,16 @@ def get_verb(verb:str):
 def get_verb_list():
     static = _get_static_folder()
     with open(f"{static}/results.json", "r") as tempFile:
-        directory = json.load(tempFile)
+        directory = json.load(tempFile) #Type: dict[str]
+
+        # turn into list of tuples
+        tuples = directory.items()
+
+        tuples = sorted(list(tuples), key=lambda verb: verb[1]['score'], reverse=True)
+
+        verb_list = [verb[0] for verb in tuples]
         
-        return [verb for verb in directory]
+        return verb_list
 
 def _get_static_folder():
-                return os.environ.get('STATIC_DIR', 'static')
+    return os.environ.get('STATIC_DIR', 'static')
