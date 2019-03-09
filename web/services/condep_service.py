d32e96ad725557f976b770749c88b6deccc67e44
Jessica Fairbairn <jess.fairbairn@postgrad.manchester.ac.uk>
2019-03-09 22:03:17 +0000

    Much improved condep lookup

--- web/services/condep_service.py
+++ web/services/condep_service.py
@@ -2,9 +2,11 @@ import typing
 try:
     from web.services import directory_service
     from web.services.verb_definition import SenseData
+    from web.services import wordnet_service
 except ImportError:
     from services import directory_service
     from services.verb_definition import SenseData
+    from services import wordnet_service
 
 import condep.definitions.verbs as condep_verbs
 import condep.definitions.synsets as condep_synsets
@@ -17,32 +19,42 @@ def get_condep_for_verb(verb:str):
     except KeyError:
         verb_details = directory_service.get_verb(verb)
         for sense_data in verb_details.database_ids: # type: SenseData
+            if not sense_data.synset:
+                continue
+
+            synset_cd = get_condep_for_synset(sense_data.synset, verb)
+            if synset_cd:
+                return synset_cd
+
             hypernyms = sense_data.hypernyms
 
             try:
-                synset = hypernyms[0] # type: str
+                hyper_set = hypernyms[0] # type: str
             except IndexError:
                 continue
 
-            synset_cd = get_condep_for_synset(synset, verb)
-            if synset_cd:
-                return synset_cd
-            else:
-                continue
+            
         return None
 
 def get_condep_for_synset(synset:str, verb:str=None):
     try:
         return condep_synsets.dictionary[synset]
     except KeyError:
-
+        # check if any of the verbs in the set have a CD definition
         verbs_in_synset = directory_service.get_verbs_in_synset(synset) # type: List[str]
-        for hyper_verb in verbs_in_synset:
-            if verb and hyper_verb == verb:
-                print('Recursive error for verb ' + verb)
+        for verb_in_set in verbs_in_synset:
+            try:
+                return condep_verbs.dictionary[verb_in_set]
+            except KeyError:
                 continue
+
+        hypernyms = wordnet_service.get_hypernyms_for_synset(synset)
+        for hyperset in hypernyms:
+            # if verb and hyper_verb == verb:
+            #     print('Recursive error for verb ' + verb)
+            #     continue
                 
-            new_cd = get_condep_for_verb(hyper_verb)
+            new_cd = get_condep_for_synset(hyperset._name)
             if new_cd:
                 return new_cd
     return None
